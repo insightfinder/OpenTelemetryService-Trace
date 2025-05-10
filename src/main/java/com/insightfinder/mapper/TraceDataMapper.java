360e72612df3a14b422aa75392d4a9282efe13cd
joeyw <wangjq199798@sina.com>
2025-05-10 15:44:10 -0400

    exclude llm judge trace

--- src/main/java/com/insightfinder/mapper/TraceDataMapper.java
+++ src/main/java/com/insightfinder/mapper/TraceDataMapper.java
@@ -60,6 +60,9 @@ public class TraceDataMapper {
       String entryOperation = rawSpans.stream()
           .min(Comparator.comparing(o -> ((JSONObject) o).getLong("startTime")))
           .map(o -> ((JSONObject) o).getString("operationName").trim()).orElse(null);
+      if (!StringUtils.isNullOrEmpty(entryOperation) && entryOperation.contains(LLM_JUDGE_URI)) {
+        return null;
+      }
       String username = null;
       for (int i = 0; i < rawSpans.size(); i++) {
         var curSpan = rawSpans.getJSONObject(i);
@@ -122,12 +125,10 @@ public class TraceDataMapper {
             promptPair.setEntryOperation(entryOperation);
           });
 
-      var traceInfoBuilder = com.insightfinder.mapper.TraceInfo.builder()
-          .promptResponsePairs(promptPairs.values().stream().toList());
-      if (!StringUtils.isNullOrEmpty(entryOperation) && entryOperation.contains(LLM_JUDGE_URI)) {
-        traceInfoBuilder.traceDataBody(traceDataBody);
-      }
-      return traceInfoBuilder.build();
+      return com.insightfinder.mapper.TraceInfo.builder()
+          .promptResponsePairs(promptPairs.values().stream().toList())
+          .traceDataBody(traceDataBody)
+          .build();
     } catch (Exception e) {
       log.error("Error mapping Jaeger raw data to IF trace: {}", e.getMessage());
       return null;
